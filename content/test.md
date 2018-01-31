4d2da072a7294dc7583b8f05bd3255bbcbeb4daf
miaoyc <miaoyongchao@haizhi.com>
2018-01-31 17:58:06 +0800

    add files

---
+++ content/test.md
@@ -0,0 +1,6 @@
+Title: test
+Date: 2018-12-03 10:20
+Category: Hahahaha
+tags: miaoyc
+
+Following is a mark down
---
+++ pelicanconf.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'miaoyc'
+SITENAME = u'miaoyc'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = u'zh'
+
+THEME = 'pelican-bootstrap3'
+
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+
+PLUGIN_PATHS = ['/Users/miaoyongchao/Desktop/miaoyc/pelican-plugins']
+
+PLUGINS = ['summary','sitemap','neighbors','tipue_search','tag_cloud', 'i18n_subsites, ''']
+DIRECT_TEMPLATES = ('index', 'categories', 'authors', 'archives', 'tags')
+
+ARCHIVES_SAVE_AS = './archives.html'
+
+PLUGINS = ['i18n_subsites']
+
+#I18N_TEMPLATES_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
